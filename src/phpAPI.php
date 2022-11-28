96a38c611a82068e05efb674184d1dfe4742ada5
Louis Ouellet <louis_ouellet@hotmail.com>
2022-11-28 14:22:17 -0500

    2022-11-28 14:22 - v1.4.5

--- src/phpAPI.php
+++ src/phpAPI.php
@@ -3,28 +3,135 @@
 //Declaring namespace
 namespace LaswitchTech\phpAPI;
 
+//Import Factory, Auth and Database classes into the global namespace
+use Composer\Factory;
+use LaswitchTech\phpDB\Database;
+use LaswitchTech\phpAUTH\Auth;
+
 class phpAPI {
 
   protected $URI;
+  protected $Path;
+  protected $Settings;
+  protected $Manifest;
+  protected $DBDebug = false;
 
   public function __construct() {
-    require __DIR__ . "/bootstrap.php";
+
+    // Include all model files
+    if(is_dir($this->Path . "/Model")){
+      foreach(scandir($this->Path . "/Model/") as $model){
+        if(str_contains($model, 'Model.php')){
+          require_once $this->Path . "/Model/" . $model;
+        }
+      }
+    }
+
+    // Parse URL
     $this->URI = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $this->URI = explode( '/', $this->URI );
     if(isset($this->URI[2],$this->URI[3])){
+
+      // Identify Controller
       $strControllerName = ucfirst($this->URI[2]) . "Controller";
       $strMethodName = $this->URI[3] . 'Action';
-      if(is_file(ROOT_PATH . "/Controller/" . $strControllerName . ".php")){
-        require ROOT_PATH . "/Controller/" . $strControllerName . ".php";
+      if(is_file($this->Path . "/Controller/" . $strControllerName . ".php")){
+
+        // Load Controller
+        require $this->Path . "/Controller/" . $strControllerName . ".php";
+
+        // Create Controller
         $objFeedController = new $strControllerName();
         $objFeedController->{$strMethodName}();
       } else {
+
+        // Could not find Controller
         header("HTTP/1.1 404 Not Found");
         exit();
       }
     } else {
-        header("HTTP/1.1 404 Not Found");
-        exit();
+
+      // Could not identify the Controller and/or Method
+      header("HTTP/1.1 404 Not Found");
+      exit();
     }
+  }
+
+  public function getPath(){
+    return $this->Path;
+  }
+
+  protected function configure(){
+
+    // Save Root Path
+    $this->Path = dirname(\Composer\Factory::getComposerFile());
+    define("ROOT_PATH", $this->Path);
+
+    // Main Auth Configuration Information
+    define("AUTH_B_TYPE", "SQL");
+    define("AUTH_RETURN", "HEADER");
+    define("AUTH_OUTPUT_TYPE", "HEADER");
+
+    // Include manifest configuration file
+    if(is_file($this->Path . "/src/manifest.json")){
+
+      // Save all settings
+      $this->Manifest = json_decode(file_get_contents($this->Path . '/src/manifest.json'),true);
+
+      // MySQL Debug
+      if(isset($this->Manifest['sql']['debug'])){
+        $this->DBDebug = $this->Manifest['sql']['debug'];
+      }
+
+      // Auth Configuration Information
+      if(isset($this->Manifest['auth']['roles'])){
+        define("AUTH_ROLES", $this->Manifest['auth']['roles']);
+      } else {
+        define("AUTH_ROLES", true);
+      }
+      if(isset($this->Manifest['auth']['groups'])){
+        define("AUTH_GROUPS", $this->Manifest['auth']['groups']);
+      } else {
+        define("AUTH_GROUPS", false);
+      }
+      if(isset($this->Manifest['auth']['type'])){
+        define("AUTH_F_TYPE", $this->Manifest['auth']['type']);
+      } else {
+        define("AUTH_F_TYPE", "BEARER");
+      }
+    } else {
+
+      // Auth Configuration Information
+      define("AUTH_ROLES", true);
+      define("AUTH_GROUPS", false);
+      define("AUTH_F_TYPE", "BEARER");
+    }
+
+    // Include main configuration file
+    if(is_file($this->Path . "/config/config.json")){
+
+      // Save all settings
+    	$this->Settings = json_decode(file_get_contents($this->Path . '/config/config.json'),true);
+
+      //MySQL Configuration Information
+      define("DB_HOST", $this->Settings['sql']['host']);
+      define("DB_USERNAME", $this->Settings['sql']['username']);
+      define("DB_PASSWORD", $this->Settings['sql']['password']);
+      define("DB_DATABASE_NAME", $this->Settings['sql']['database']);
+
+      // MySQL Debug
+      if(isset($this->Settings['sql']['debug'])){
+        $this->DBDebug = $this->Settings['sql']['debug'];
+      }
+    } else {
+
+      // Could not find settings
+      header_remove('Set-Cookie');
+      header('HTTP/1.1 500 Internal Server Error');
+      exit;
+    }
+
+    // MySQL Debug
+    define("DB_DEBUG", $this->DBDebug);
   }
 }
