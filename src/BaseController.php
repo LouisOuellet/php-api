18a09093843eb7a29b429e7624f3b124d5daaa48
Louis Ouellet <louis_ouellet@hotmail.com>
2022-11-28 08:29:56 -0500

    2022-11-28 08:29 - v1.4.2

--- src/BaseController.php
+++ src/BaseController.php
@@ -23,7 +23,7 @@ class BaseController {
   protected function getQueryStringBody() {
     $query = [];
     foreach($_POST as $key => $value){
-      $query[$key] = base64_decode(urldecode($value))
+      $query[$key] = base64_decode(urldecode($value));
     }
     return $query;
   }
