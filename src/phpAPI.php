7b293ac3d92a56d01c3a96a44b01d2465c71939e
Louis Ouellet <louis_ouellet@hotmail.com>
2022-12-14 19:06:49 -0500

    2022-12-14 19:06 - v1.4.16

--- src/phpAPI.php
+++ src/phpAPI.php
@@ -82,7 +82,7 @@ class phpAPI {
 
     // Save Root Path
     $this->Path = dirname(\Composer\Factory::getComposerFile());
-    define("ROOT_PATH", $this->Path);
+    if(!defined("ROOT_PATH")){ define("ROOT_PATH", $this->Path); }
 
     // Include manifest configuration file
     if(is_file($this->Path . "/src/manifest.json")){
@@ -97,26 +97,26 @@ class phpAPI {
 
       // Auth Configuration Information
       if(isset($this->Manifest['auth']['roles'])){
-        define("AUTH_ROLES", $this->Manifest['auth']['roles']);
+        if(!defined("AUTH_ROLES")){ define("AUTH_ROLES", $this->Manifest['auth']['roles']); }
       } else {
-        define("AUTH_ROLES", true);
+        if(!defined("AUTH_ROLES")){ define("AUTH_ROLES", true); }
       }
       if(isset($this->Manifest['auth']['groups'])){
-        define("AUTH_GROUPS", $this->Manifest['auth']['groups']);
+        if(!defined("AUTH_GROUPS")){ define("AUTH_GROUPS", $this->Manifest['auth']['groups']); }
       } else {
-        define("AUTH_GROUPS", false);
+        if(!defined("AUTH_GROUPS")){ define("AUTH_GROUPS", false); }
       }
       if(isset($this->Manifest['auth']['type']['api'])){
-        define("AUTH_F_TYPE", $this->Manifest['auth']['type']['api']);
+        if(!defined("AUTH_F_TYPE")){ define("AUTH_F_TYPE", $this->Manifest['auth']['type']['api']); }
       } else {
-        define("AUTH_F_TYPE", "BEARER");
+        if(!defined("AUTH_F_TYPE")){ define("AUTH_F_TYPE", "BEARER"); }
       }
     } else {
 
       // Auth Configuration Information
-      define("AUTH_ROLES", true);
-      define("AUTH_GROUPS", false);
-      define("AUTH_F_TYPE", "BEARER");
+      if(!defined("AUTH_ROLES")){ define("AUTH_ROLES", true); }
+      if(!defined("AUTH_GROUPS")){ define("AUTH_GROUPS", false); }
+      if(!defined("AUTH_F_TYPE")){ define("AUTH_F_TYPE", "BEARER"); }
     }
 
     // Include main configuration file
@@ -127,24 +127,15 @@ class phpAPI {
 
       //MySQL Configuration Information
       if(isset($this->Settings['sql'])){
-        define("DB_HOST", $this->Settings['sql']['host']);
-        define("DB_USERNAME", $this->Settings['sql']['username']);
-        define("DB_PASSWORD", $this->Settings['sql']['password']);
-        define("DB_DATABASE_NAME", $this->Settings['sql']['database']);
+        if(!defined("DB_HOST")){ define("DB_HOST", $this->Settings['sql']['host']); }
+        if(!defined("DB_USERNAME")){ define("DB_USERNAME", $this->Settings['sql']['username']); }
+        if(!defined("DB_PASSWORD")){ define("DB_PASSWORD", $this->Settings['sql']['password']); }
+        if(!defined("DB_DATABASE_NAME")){ define("DB_DATABASE_NAME", $this->Settings['sql']['database']); }
 
         // MySQL Debug
         if(isset($this->Settings['sql']['debug'])){
           $this->Debug = $this->Settings['sql']['debug'];
         }
-      }
-
-      //SMTP Configuration Information
-      if(isset($this->Settings['smtp'])){
-        define("SMTP_HOST", $this->Settings['smtp']['host']);
-        define("SMTP_PORT", $this->Settings['smtp']['port']);
-        define("SMTP_ENCRYPTION", $this->Settings['smtp']['encryption']);
-        define("SMTP_USERNAME", $this->Settings['smtp']['username']);
-        define("SMTP_PASSWORD", $this->Settings['smtp']['password']);
       }
     } else {
 
@@ -153,6 +144,6 @@ class phpAPI {
     }
 
     // MySQL Debug
-    define("DB_DEBUG", $this->Debug);
+    if(!defined("DB_DEBUG")){ define("DB_DEBUG", $this->Debug); }
   }
 }
