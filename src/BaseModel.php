5ebe63a8bdd6f28015bf07481e9e03bdfd08466a
Louis Ouellet <louis_ouellet@hotmail.com>
2023-04-13 11:48:18 -0400

    2023-04-13 11:48 - v2.0.2

--- src/BaseModel.php
+++ src/BaseModel.php
@@ -3,28 +3,10 @@
 //Declaring namespace
 namespace LaswitchTech\phpAPI;
 
-// Import phpConfigurator class into the global namespace
-use LaswitchTech\phpConfigurator\phpConfigurator;
-
-// Import phpLogger class into the global namespace
-use LaswitchTech\phpLogger\phpLogger;
-
 //Import Database class into the global namespace
 use LaswitchTech\phpDB\Database;
 
 class BaseModel extends Database {
-
-	// phpLogger
-	protected $Logger;
-
-  // phpConfigurator
-  protected $Configurator = null;
-
-  public function __construct(){
-
-    // Initiate Database
-    parent::__construct();
-  }
 
   public function __call($name, $arguments) {
 
