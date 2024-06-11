d15a4a11777e494c65d7ec0eb7e1941dfd99b663
Louis Ouellet <louis_ouellet@hotmail.com>
2024-06-11 08:38:07 -0400

    Added an isInstalled method and some Code Cleanup

--- src/phpAPI.php
+++ src/phpAPI.php
@@ -14,91 +14,119 @@ use LaswitchTech\phpAUTH\phpAUTH;
 
 class phpAPI {
 
-	// phpLogger
-	private $Logger;
+    // phpLogger
+    private $Logger;
 
-  // phpConfigurator
-  private $Configurator = null;
+    // phpConfigurator
+    private $Configurator = null;
 
-	// phpAUTH
-  private $Auth = null;
+    // phpAUTH
+    private $Auth = null;
 
-  // URI
-  private $URI;
+    // URI
+    private $URI;
 
-  public function __construct() {
+    public function __construct() {
 
-    // Initialize Configurator
-    $this->Configurator = new phpConfigurator('api');
+        // Initialize Configurator
+        $this->Configurator = new phpConfigurator('api','requirements');
 
-    // Initiate phpLogger
-    $this->Logger = new phpLogger('api');
+        // Initiate phpLogger
+        $this->Logger = new phpLogger('api');
 
-    // Initiate phpAuth
-    $this->Auth = new phpAuth();
+        // Initiate phpAuth
+        $this->Auth = new phpAuth();
 
-    // Include all model files
-    if(is_dir($this->Configurator->root() . "/Model")){
-      foreach(scandir($this->Configurator->root() . "/Model/") as $model){
-        if(str_contains($model, 'Model.php')){
-          require_once $this->Configurator->root() . "/Model/" . $model;
+        // Include all model files
+        if(is_dir($this->Configurator->root() . "/Model")){
+            foreach(scandir($this->Configurator->root() . "/Model/") as $model){
+                if(str_contains($model, 'Model.php')){
+                    require_once $this->Configurator->root() . "/Model/" . $model;
+                }
+            }
         }
-      }
+
+        // Parse URL
+        $this->URI = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $this->URI = explode( '/', $this->URI );
+        if(isset($this->URI[2],$this->URI[3])){
+
+            // Identify Controller
+            $strControllerName = ucfirst($this->URI[2]) . "Controller";
+            $strMethodName = $this->URI[3] . 'Action';
+            if(is_file($this->Configurator->root() . "/Controller/" . $strControllerName . ".php")){
+
+                // Load Controller
+                require $this->Configurator->root() . "/Controller/" . $strControllerName . ".php";
+
+                // Create Controller
+                $objFeedController = new $strControllerName($this->Auth);
+
+                // Call Method
+                $objFeedController->{$strMethodName}();
+            } else {
+
+                // Could not find Controller
+                $this->sendOutput('Could not find Controller', array('HTTP/1.1 404 Not Found'));
+            }
+        } else {
+
+            // Could not identify the Controller and/or Method
+            $this->sendOutput('Could not identify the Controller and/or Action', array('HTTP/1.1 422 Unprocessable Entity'));
+        }
     }
 
-    // Parse URL
-    $this->URI = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    $this->URI = explode( '/', $this->URI );
-    if(isset($this->URI[2],$this->URI[3])){
+    private function sendOutput($data, $httpHeaders=array()) {
 
-      // Identify Controller
-      $strControllerName = ucfirst($this->URI[2]) . "Controller";
-      $strMethodName = $this->URI[3] . 'Action';
-      if(is_file($this->Configurator->root() . "/Controller/" . $strControllerName . ".php")){
+        // Remove the default Set-Cookie header
+        header_remove('Set-Cookie');
 
-        // Load Controller
-        require $this->Configurator->root() . "/Controller/" . $strControllerName . ".php";
+        // Add the custom headers
+        if (is_array($httpHeaders) && count($httpHeaders)) {
+            foreach ($httpHeaders as $httpHeader) {
+                header($httpHeader);
+            }
+        }
 
-        // Create Controller
-        $objFeedController = new $strControllerName($this->Auth);
+        // Check if the data is an array or object
+        if(is_array($data) || is_object($data)){
 
-        // Call Method
-        $objFeedController->{$strMethodName}();
-      } else {
+            // Convert the data to JSON
+            $data = json_encode($data,JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        }
 
-        // Could not find Controller
-        $this->sendOutput('Could not find Controller', array('HTTP/1.1 404 Not Found'));
-      }
-    } else {
+        // Send the output
+        echo $data;
 
-      // Could not identify the Controller and/or Method
-      $this->sendOutput('Could not identify the Controller and/or Action', array('HTTP/1.1 422 Unprocessable Entity'));
-    }
-  }
-
-  private function sendOutput($data, $httpHeaders=array()) {
-
-    // Remove the default Set-Cookie header
-    header_remove('Set-Cookie');
-
-    // Add the custom headers
-    if (is_array($httpHeaders) && count($httpHeaders)) {
-      foreach ($httpHeaders as $httpHeader) {
-        header($httpHeader);
-      }
+        // Exit the script
+        exit;
     }
 
-    // Check if the data is an array or object
-    if(is_array($data) || is_object($data)){
+    protected function isInstalled(){
 
-      // Convert the data to JSON
-      $data = json_encode($data,JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        // Retrieve the list of required modules
+        $modules = $this->Configurator->get('requirements','modules');
+
+        // Check if the required modules are installed
+        foreach($modules as $module){
+
+            // Check if the class exists
+            if (!class_exists($module)) {
+                return false;
+            }
+
+            // Initialize the class
+            $class = new $module();
+
+            // Check if the method exists
+            if(method_exists($class, isInstalled)){
+                if(!$class->isInstalled()){
+                    return false;
+                }
+            }
+        }
+
+        // Return true
+        return true;
     }
-
-    // Send the output
-    echo $data;
-
-    // Exit the script
-    exit;
-  }
 }
